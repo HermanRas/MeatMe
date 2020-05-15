5d1c0f6152e8865bc0f459562cf5d05fcd7343b1
Herman Ras <hermanr@PetraGroup.local>
2020-05-15 19:59:12 +0200

    fixed spacing

--- _footer.php
+++ _footer.php
@@ -1,7 +1,7 @@
 <div style="min-height:100px" class="text-center rounded bg-dark">
-    <a class="btn btn-dark"><i class="text-white display-4 fab fa-facebook-square"></i></a>
-    <a class="btn btn-dark"><i class="text-white display-4 fab fa-whatsapp-square"></i></a>
-    <a class="btn btn-dark"><i class="text-white display-4 fab fa-pinterest-square"></i></a>
-    <a class="btn btn-dark"><i class="text-white display-4 fab fa-twitter-square"></i></a>
-    <a class="btn btn-dark"><i class="text-white display-4 fab fa-linkedin"></i></a>
+    <a class="btn btn-dark p-1"><i class="text-white display-4 fab fa-facebook-square"></i></a>
+    <a class="btn btn-dark p-1"><i class="text-white display-4 fab fa-whatsapp-square"></i></a>
+    <a class="btn btn-dark p-1"><i class="text-white display-4 fab fa-pinterest-square"></i></a>
+    <a class="btn btn-dark p-1"><i class="text-white display-4 fab fa-twitter-square"></i></a>
+    <a class="btn btn-dark p-1"><i class="text-white display-4 fab fa-linkedin"></i></a>
 </div>
