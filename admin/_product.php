4a8d1b50dc1b36b142180ada04cc7ee6e268f625
Herman Ras <hermanr@PetraGroup.local>
2020-05-25 19:29:47 +0200

    reworked item page for editing

--- admin/_product.php
+++ admin/_product.php
@@ -1,17 +1,70 @@
 <?php
+///////////////////////////////////////////////////////////////////////////////////
+// Edit Item
+///////////////////////////////////////////////////////////////////////////////////
     if(isset($_POST['save'])){
-
-        echo "<h1> One Day this will save your data </h1>";
+        echo "<h1> One Day this will edit your data </h1>";
         var_dump($_POST);
         echo '<a href="home.php" class="btn btn-lg btn-primary w-100"> CANCEL </a>';
         die;
     }
 
-    if(isset($_POST['product'])){
+///////////////////////////////////////////////////////////////////////////////////
+// Add Item
+///////////////////////////////////////////////////////////////////////////////////
+    if(isset($_POST['add'])){
+        echo "<h1> One Day this will add your data </h1>";
+        var_dump($_POST);
+        echo '<a href="home.php" class="btn btn-lg btn-primary w-100"> CANCEL </a>';
+        die;
+    }
+
+
+///////////////////////////////////////////////////////////////////////////////////
+// DELETE Item
+///////////////////////////////////////////////////////////////////////////////////
+    if(isset($_POST['delete'])){
+        echo "<h1> One Day this will delete your data </h1>";
+        var_dump($_POST);
+        echo '<a href="home.php" class="btn btn-lg btn-primary w-100"> CANCEL </a>';
+        die;
+    }
+
+
+///////////////////////////////////////////////////////////////////////////////////
+// Load Item for edit
+///////////////////////////////////////////////////////////////////////////////////
+    if(isset($_POST['product']) & isset($_POST['item'])){
+        //set defaults 
         $product = $_POST['product'];
+        $item = $_POST['item'];
         $jsonfile = file_get_contents("../data/$product");
         $json_data = json_decode($jsonfile, true);
+
+        // if new show blank form, else show update
+        if($item !== 'ADD'){   
+            //user is updating
+            $i = 0;
+            foreach ($json_data['items'] as $value) {
+                if($value['name'] === $item){
+                    $item = $value;
+                break;
+                }
+            $i++;
+            }
+        }else{
+            $i = '';
+            $item = [];
+            $item['name'] = '';
+            $item['desc'] = '';
+            $item['IMG'] = '';
+            $item['Price p/kg'] = '';
+            $item['Portion Size'] = [['100'=>'100g']];
+            $item['minQTY'] = 0;
+            $item['maxQTY'] = 1000;
+        }
     }else{    
+        // if open the page opens directly send user to select screen
         echo '<script>window.location.replace("products.php");</script>';
         die;
     }
@@ -20,29 +73,33 @@
 <div class="container mt-3">
     <h1 class="bg-secondary-dark rounded p-2 text-center">Products</h1>
     <form method="POST" action="product.php">
-        <input required type="hidden" value="<?php echo $item['product']; ?>">
-        <?php
-            //Display Items
-            foreach ($json_data["items"] as $item) {
-        ?>
+        <input type="hidden" name="update" value="<?php echo $i; ?>">
+        <input type="hidden" name="product" value="<?php echo $product; ?>">
         <h2 class="bg-primary-dark text-center text-white"><?php echo $item['desc']; ?></h2>
         <div class="form-group">
             <label for="name">Name</label>
-            <input required type="text" class="form-control" id="name" name="name[]"
-                value="<?php echo $item['name']; ?>">
+            <input required type="text" class="form-control" id="name" name="name" value="<?php echo $item['name']; ?>">
         </div>
         <div class="form-group">
             <label for="desc">Description</label>
-            <input required type="text" class="form-control" id="desc" name="desc[]"
-                value="<?php echo $item['desc']; ?>">
+            <input required type="text" class="form-control" id="desc" name="desc" value="<?php echo $item['desc']; ?>">
         </div>
         <div class="form-group">
             <label for="img">Image</label>
-            <input required type="text" class="form-control" id="img" name="img[]" value="<?php echo $item['IMG']; ?>">
+            <select class="form-control" id="img" name="img" onchange="updateIMG()">
+                <option value="<?php echo $item['IMG']; ?>"><?php echo $item['IMG']; ?></option>
+                <?php
+                 $files = array_diff(scandir('../img/'.str_replace('.json','',$product)), array('..', '.'));
+                 foreach ($files as $file) {
+                     echo '<option value="'.'../img/'.str_replace('.json','',$product).'/'.$file.'">'.$file.'</option>';
+                    }
+                    ?>
+            </select>
+            <img class="rounded p-2" id="itemPic" width="250px" src="../<?php echo $item['IMG']; ?>" alt="item picture">
         </div>
         <div class="form-group">
             <label for="price">Price Per Kg</label>
-            <input required type="text" class="form-control" id="price" name="price[]"
+            <input required type="text" class="form-control" id="price" name="price"
                 value="<?php echo $item['Price p/kg']; ?>">
         </div>
         <div class="form-group">
@@ -56,41 +113,49 @@
             }
             ?>
             <small id="sizeHelp" class="form-text text-muted">Total Weight grams = Displayed Portion text</small>
-            <textarea required class="form-control" id="size" name="size[]" rows="3"
+            <textarea required class="form-control" id="size" name="size" rows="3"
                 aria-describedby="sizeHelp"><?php echo $size; ?></textarea>
         </div>
         <div class="form-group">
             <label for="minQty">Minimum Quantity</label>
-            <input required type="number" class="form-control" id="minQty" name="minQty[]"
+            <input required type="number" class="form-control" id="minQty" name="minQty"
                 value="<?php echo $item['minQTY']; ?>">
         </div>
         <div class="form-group">
             <label for="maxQty">Maximum Quantity</label>
-            <input required type="number" class="form-control" id="maxQty" name="maxQty[]"
+            <input required type="number" class="form-control" id="maxQty" name="maxQty"
                 value="<?php echo $item['maxQTY']; ?>">
         </div>
+
         <?php
-            echo "<br><hr>";
-            }
+            if ($i !== ''){
         ?>
-
-        <div id="productFrm">
-            <!-- new elements here -->
-        </div>
-
         <div class="row text-center">
             <div class="col-12 col-md-4">
-                <input type="submit" class="btn btn-lg btn-secondary w-100" name="save" value=" SAVE ">
+                <input type="submit" class="btn btn-lg btn-secondary w-100" name="save" value="Update">
             </div>
             <div class="col-12 col-md-4">
-                <button type="button" class="btn btn-lg btn-dark w-100" onclick="addOne()"> Add </button>
+                <input type="submit" class="btn btn-lg btn-dark w-100" name="delete" value="Delete">
             </div>
             <div class="col-12 col-md-4">
-                <a href="home.php" class="btn btn-lg btn-primary w-100"> CANCEL </a>
+                <a href="home.php" class="btn btn-lg btn-primary w-100"> Cancel </a>
             </div>
         </div>
+        <?php
+            }else{
+        ?>
+        <div class="row text-center">
+            <div class="col-12 col-md-6">
+                <input type="submit" class="btn btn-lg btn-secondary w-100" name="add" value="Add">
+            </div>
+            <div class="col-12 col-md-6">
+                <a href="home.php" class="btn btn-lg btn-primary w-100"> Cancel </a>
+            </div>
+        </div>
+        <?php
+            }
+        ?>
         <hr>
     </form>
 </div>
-<!-- page Script -->
 <script src="js/catagoryItems.js"></script>
--- admin/js/catagoryItems.js
+++ admin/js/catagoryItems.js
@@ -1,46 +1,12 @@
-function addOne() {
+function updateIMG() {
+    const x = document.getElementById("img").selectedIndex;
+    const y = document.getElementById("img").options;
+    const imgURL = y[x].value;
 
-    let newElement = `
-        <h2 class="bg-primary-dark text-center text-white">New Product:</h2>
-        <div class="form-group">
-            <label for="name">Name</label>
-            <input type="text" class="form-control" id="name" name="name[]" value="">
-        </div>
-        <div class="form-group">
-            <label for="desc">Description</label>
-            <input type="text" class="form-control" id="desc" name="desc[]" value="">
-        </div>
-        <div class="form-group">
-            <label for="img">Image</label>
-            <input type="text" class="form-control" id="img" name="img[]" value="">
-        </div>
-        <div class="form-group">
-            <label for="price">Price Per Kg</label>
-            <input type="text" class="form-control" id="price" name="price[]"
-                value="">
-        </div>
-        <div class="form-group">
-            <label for="size">Portion Size</label>
-            <small id="sizeHelp" class="form-text text-muted">Total Weight grams = Displayed Portion text</small>
-            <textarea class="form-control" id="size" name="size[]" rows="3"
-                aria-describedby="sizeHelp">0=0g</textarea>
-        </div>
-        <div class="form-group">
-            <label for="minQty">Minimum Quantity</label>
-            <input type="number" class="form-control" id="minQty" name="minQty[]"
-                value="0">
-        </div>
-        <div class="form-group">
-            <label for="maxQty">Maximum Quantity</label>
-            <input type="number" class="form-control" id="maxQty" name="maxQty[]"
-                value="1000">
-        </div>`;
+    const itemPic = document.getElementById('itemPic');
+    itemPic.src = imgURL;
 
 
-    var newChild = document.createElement('div');
-    newChild.innerHTML = newElement;
-    // child = child.firstChild;
-    document.getElementById('productFrm').appendChild(newChild);
 }
 
 function itemUpdate() {
@@ -54,4 +20,5 @@ function itemUpdate() {
     items.forEach(item => {
         selectItem.innerHTML = selectItem.innerHTML + `<option value="` + item.name + `">` + item.desc + `</option>`
     });
+    selectItem.innerHTML = selectItem.innerHTML + `<option value="ADD">ADD New Item</option>`
 }
