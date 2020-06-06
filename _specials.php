29efd2663de3c24176781d04c4e1c3062977efa5
Herman Ras <hermanr@PetraGroup.local>
2020-06-06 09:48:22 +0200

    load specials from file

--- _specials.php
+++ _specials.php
@@ -1,29 +1,24 @@
+<?php
+// Load all Json files and Remove .. & . files
+    $files = array_diff(scandir('img/Special'), array('..', '.'));
+?>
 <!-- PageStart -->
 <main data-barba="container" data-barba-namespace="home">
     <div class="container mt-3">
         <hr>
         <h1 class="bg-secondary-dark rounded p-2">Specials !</h1>
         <div class="row">
+            <?php
+            //loop thru files as categories
+            foreach ($files as $file) {
+            ?>
             <div class="col-12 col-md-6 text-center">
                 <a href="#" class="btn btn-outline-secondary"><img class="img-fluid"
-                        src="https://via.placeholder.com/1024x1024.png?text=1024x1024 + ADD4"
-                        class="rounded main-ico"></a>
+                        src="img/Special/<?php echo $file ;?>" class="rounded main-ico"></a>
             </div>
-            <div class="col-12 col-md-6 text-center">
-                <a href="#" class="btn btn-outline-secondary"><img class="img-fluid"
-                        src="https://via.placeholder.com/1024x1024.png?text=1024x1024 + ADD5"
-                        class="rounded main-ico"></a>
-            </div>
-            <div class="col-12 col-md-6 text-center">
-                <a href="#" class="btn btn-outline-secondary"><img class="img-fluid"
-                        src="https://via.placeholder.com/1024x1024.png?text=1024x1024 + ADD6"
-                        class="rounded main-ico"></a>
-            </div>
-            <div class="col-12 col-md-6 text-center">
-                <a href="#" class="btn btn-outline-secondary"><img class="img-fluid"
-                        src="https://via.placeholder.com/1024x1024.png?text=1024x1024 + ADD7"
-                        class="rounded main-ico"></a>
-            </div>
+            <?php
+            }
+            ?>
         </div>
         <br>
         <hr>
