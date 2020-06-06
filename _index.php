e6ad8c75bd1181119829cd7ab58e2a5f8167d681
Herman Ras <hermanr@PetraGroup.local>
2020-06-06 10:02:20 +0200

    added slider from file

--- _index.php
+++ _index.php
@@ -1,44 +1,7 @@
 <div class="container mt-3">
     <img src="img/icon.png" class="index-img d-block d-sm-none mx-auto rounded">
     <div class="d-none d-sm-block">
-        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-            <ol class="carousel-indicators">
-                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-            </ol>
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <img class="d-block w-100" src="img/Slider/1920x500.png" alt="First slide">
-                    <div class="carousel-caption d-none d-md-block">
-                        <h5>Deli Meat!</h5>
-                        <p>Any time is snack time !</p>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <img class="d-block w-100" src="img/Slider/1920x500.png" alt="Second slide">
-                    <div class="carousel-caption d-none d-md-block">
-                        <h5>Fresh Raw Meat</h5>
-                        <p>Some Text ....... and some more</p>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <img class="d-block w-100" src="img/Slider/1920x500.png" alt="Third slide">
-                    <div class="carousel-caption d-none d-md-block">
-                        <h5>Game Processing</h5>
-                        <p>You shoot it, we pack it!</p>
-                    </div>
-                </div>
-            </div>
-            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-            </a>
-        </div>
+        <?php include_once("_Slider.php")?>
     </div>
     <hr>
     <!-- <h1 class="bg-secondary-dark rounded p-2 text-center">QWEENS <br><small>ONLINE DELI</small></h1> -->
