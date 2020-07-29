f0cb33f795a6c5b160af6dea8b448138a8c183e3
Herman Ras <hermanr@PetraGroup.local>
2020-07-29 14:34:10 +0200

    updated Ozow integrations

--- payOnline.php
+++ payOnline.php
@@ -1,20 +1,63 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php 
+$orderID = '';
+$val = '';
 
-<head>
-    <!-- Add header -->
-    <?php include_once('_head.php'); ?>
-</head>
+if(isset($_GET['Val']) && isset($_GET['orderID'])){
+    $orderID = $_GET['orderID'];
+    $val = $_GET['Val'];
+}else{
+    echo  '{"error": "OrderFailed"}';
+    die;
+}
 
-<body data-barba="wrapper">
-    <!-- PageStart -->
-    <main data-barba="container" data-barba-namespace="home">
-        <?php include_once('_payOnline.php'); ?>
-    </main>
+require_once('config/db.php');
 
-    <!-- include Scipts -->
-    <?php include_once('_script.php') ?>
+$postData = array('SiteCode' => $siteCode,
+                    'CountryCode' => 'ZA',
+                    'CurrencyCode' => 'ZAR',
+                    'Amount' => $val,
+                    'TransactionReference' => $orderID,
+                    'BankReference' => 'QueensOD-'.$orderID,
+                    'CancelUrl' => 'https://dragoon.co.za/QweensOnlineDeli/notice.php?CANCEL',
+                    'ErrorUrl' => 'https://dragoon.co.za/QweensOnlineDeli/notice.php?ERROR',
+                    'SuccessUrl' => 'https://dragoon.co.za/QweensOnlineDeli/notice.php?SUCCESS',
+                    'NotifyUrl' => 'https://dragoon.co.za/QweensOnlineDeli/notice.php?NOTICE',
+                    'IsTest' => 'true');
 
-</body>
+$hashString = strtolower(implode('', $postData) . $privateKey);
+$hashCheck = hash('sha512', $hashString);
+$postData['HashCheck'] = $hashCheck;
+$ozowResult = getPaymentLinkModel($postData, $apiKey);
 
-</html>
+if (!empty($ozowResult->errorMessage)) {
+    die($ozowResult->errorMessage);
+}
+
+//echo $ozowResult->url;
+header('Location:'. $ozowResult->url, true);
+die();
+
+function getPaymentLinkModel($postData, $apiKey)
+{
+    $jsonRequest = json_encode($postData);
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        'Accept: application/json',
+        'ApiKey:' . $apiKey,
+        'Content-Type: application/json'
+    ));
+
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonRequest);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_URL, 'https://api.ozow.com/postpaymentrequest');
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    $requestResult = curl_exec($ch);
+
+    if ($requestResult === false) {
+        die('Error generating Ozow URL: curl error');
+    }
+    
+    return json_decode($requestResult);
+}
